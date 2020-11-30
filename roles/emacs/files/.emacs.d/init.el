a34e80e09bc6a84b5f9caedd4b8b07f50e5b13b9
Mirco De Zorzi <mircodezorzi@protonmail.com>
2020-11-30 23:53:54 +0100

    feat(emacs): still unsure on which editor to settle with...

--- roles/emacs/files/.emacs.d/init.el
+++ roles/emacs/files/.emacs.d/init.el
@@ -16,15 +16,19 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("3380a2766cf0590d50d6366c5a91e976bdc3c413df963a0ab9952314b4577299" default))
+	 '("2f1518e906a8b60fac943d02ad415f1d8b3933a5a7f75e307e6e9a26ef5bf570" "3380a2766cf0590d50d6366c5a91e976bdc3c413df963a0ab9952314b4577299" default))
  '(package-selected-packages
-   '(helm-swoop company-math company-auctex vterm yaml-mode general workgroups2 highlight-indent-guides go-guru go-eldoc company-go go-mode helm-ag helm-projectile projectile lsp-python-ms helm evil ccls company-lsp lsp-ui lsp-mode use-package)))
+	 '(org-bullets transient magit ivy-xref counsel-projectile counsel undo-tree org-make-toc ox ox-hugo helm-swoop company-math company-auctex vterm yaml-mode general workgroups2 highlight-indent-guides go-guru go-eldoc company-go go-mode helm-ag helm-projectile projectile lsp-python-ms helm evil ccls company-lsp lsp-ui lsp-mode use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(TeX-fold-folded-face ((t (:inherit normal)))))
+ '(TeX-fold-folded-face ((t (:inherit normal))))
+ '(org-level-1 ((t (:weight bold :height 1.1))))
+ '(org-level-2 ((t (:weight bold :height 1.07))))
+ '(org-level-3 ((t (:weight bold :height 1.05))))
+ '(org-level-4 ((t (:weight bold :height 1.02)))))
 
 (require 'org)
 
--- roles/emacs/readme.md
+++ roles/emacs/readme.md
@@ -1,1 +1,1 @@
-[](https://raw.githubusercontent.com/mircodezorzi/dotfiles/master/screenshots/201102-1200-31.png)
+![](https://raw.githubusercontent.com/mircodezorzi/dotfiles/master/screenshots/201102-1200-31.png)
